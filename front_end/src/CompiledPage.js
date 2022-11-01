23aae05d924f9f7a1d58505d425315e4c5dbab49
tangym27 <mt4119@nyu.edu>
2022-11-01 13:24:51 -0400

    organized compiled page

--- front_end/src/CompiledPage.js
+++ front_end/src/CompiledPage.js
@@ -1,5 +1,6 @@
 import Carousel from "react-material-ui-carousel";
-import { Paper, Button } from "@mui/material";
+import { Paper } from "@mui/material";
+import Button from "react-bootstrap/Button";
 
 const gin = require("./img/gin.png");
 const brandy = require("./img/brandy.png");
@@ -16,6 +17,8 @@ const divStyle = {
   margin: "40px",
   height: "80vh",
   border: "15px solid #fda0d8",
+  backgroundColor: "pink",
+  width: "50vh",
 };
 const pStyle = {
   margin: "10px",
@@ -25,7 +28,10 @@ const pStyle = {
 };
 
 const pageStyle = {
-  backgroundColor: "pink",
+  height: "100vh",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 };
 
 const buttonStyle = {
@@ -42,72 +48,89 @@ const carouselStyle = {
 
 const imgStyle = {
   width: "80px",
-  height: "120px",
+  height: "140px",
 };
-function Example(props) {
-  var items = [
-    {
-      name: "Gin",
-      taste: "Bitter taste",
-      percent: "35% - 55%",
-      image: gin,
-    },
-    {
-      name: "Vodka",
-      taste: "Bitter taste",
-      percent: "40%",
-      image: vodka,
-    },
-    {
-      name: "Whiskey",
-      taste: "Bittersweet",
-      percent: "40%-50%",
-      image: whiskey,
-    },
-    {
-      name: "Rum",
-      taste: "Sweet",
-      percent: "40% - 75.5%",
-      image: rum,
-    },
-    {
-      name: "Tequila",
-      taste: "Sweet",
-      percent: "40%",
-      image: tequila,
-    },
-    {
-      name: "Hard Seltzer",
-      taste: "Sweet/fruity",
-      percent: "4%-8%",
-      image: hardSeltzer,
-    },
-    {
-      name: "Beer",
-      taste: "Bitter",
-      percent: "4%-5%",
-      image: beer,
-    },
-    {
-      name: "Red Wine",
-      taste: "Sweet",
-      percent: "5-14%",
-      image: redWine,
-    },
-    {
-      name: "White Wine",
-      taste: "Sweet",
-      percent: "5-14%",
-      image: whiteWine,
-    },
-    {
-      name: "Brandy",
-      taste: "Sweet",
-      percent: "35-60%",
-      image: brandy,
-    },
-  ];
 
+var bitterItems = [
+  {
+    name: "Gin",
+    taste: "Bitter",
+    percent: "35% - 55%",
+    image: gin,
+    price: "$$",
+  },
+  {
+    name: "Vodka",
+    taste: "Bitter",
+    percent: "40%",
+    image: vodka,
+    price: "$$",
+  },
+
+  {
+    name: "Beer",
+    taste: "Bitter",
+    percent: "4%-5%",
+    image: beer,
+    price: "$",
+  },
+];
+
+var otherItems = [
+  {
+    name: "Whiskey",
+    taste: "Bittersweet",
+    percent: "40%-50%",
+    image: whiskey,
+    price: "$$",
+  },
+  {
+    name: "Hard Seltzer",
+    taste: "Sweet/fruity",
+    percent: "4%-8%",
+    image: hardSeltzer,
+    price: "$",
+  },
+];
+var sweetItems = [
+  {
+    name: "Rum",
+    taste: "Sweet",
+    percent: "40% - 75.5%",
+    image: rum,
+    price: "$$",
+  },
+  {
+    name: "Tequila",
+    taste: "Sweet",
+    percent: "40%",
+    image: tequila,
+    price: "$",
+  },
+
+  {
+    name: "Red Wine",
+    taste: "Sweet",
+    percent: "5-14%",
+    image: redWine,
+    price: "$$",
+  },
+  {
+    name: "White Wine",
+    taste: "Sweet",
+    percent: "5-14%",
+    image: whiteWine,
+    price: "$$",
+  },
+  {
+    name: "Brandy",
+    taste: "Sweet",
+    percent: "35-60%",
+    image: brandy,
+    price: "$$",
+  },
+];
+function Example(content) {
   return (
     <Carousel
       NavButton={({ onClick, className, style, next, prev }) => {
@@ -121,7 +144,7 @@ function Example(props) {
       NextIcon="next" // Change the "inside" of the next button to "next"
       PrevIcon="prev" // Change the "inside of the prev button to "prev"
     >
-      {items.map((item, i) => (
+      {content.map((item, i) => (
         <Item key={i} item={item} />
       ))}
     </Carousel>
@@ -134,15 +157,46 @@ function Item(props) {
       <h2>{props.item.name}</h2>
       <p>{props.item.taste}</p>
       <p>{props.item.percent}</p>
+      <p>{props.item.price}</p>
+
       <img style={imgStyle} src={props.item.image} />
+      <br />
     </div>
   );
 }
 
 const HomePage = () => (
-  <div style={divStyle}>
-    <br /> <br />
-    <div style={carouselStyle}>{Example()}</div>
+  <div style={pageStyle}>
+    <div style={divStyle}>
+      <br />
+      <h1> Sweet Drinks </h1>
+      <br />
+      <div style={carouselStyle}>{Example(sweetItems)}</div>
+      <hr />
+      <Button as="a" variant="outline-secondary" size="lg" href="#started">
+        Learn More
+      </Button>
+    </div>
+    <div style={divStyle}>
+      <br />
+      <h1> Bitter Drinks </h1>
+      <br />
+      <div style={carouselStyle}>{Example(bitterItems)}</div>
+      <hr />
+      <Button as="a" variant="outline-secondary" size="lg" href="#started">
+        Learn More
+      </Button>
+    </div>
+    <div style={divStyle}>
+      <br />
+      <h1> Other Drinks </h1>
+      <br />
+      <div style={carouselStyle}>{Example(otherItems)}</div>
+      <hr />
+      <Button as="a" variant="outline-secondary" size="lg" href="#started">
+        Learn More
+      </Button>
+    </div>
   </div>
 );
 
