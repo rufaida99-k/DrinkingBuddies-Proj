5f66b3bd2b9219395ef73c8ff26b650e8e5d82ac
tangym27 <mt4119@nyu.edu>
2022-11-20 18:59:11 -0500

    faq page

---
+++ front_end/src/FAQData.js
@@ -0,0 +1,87 @@
+const GeneralData = {
+  rows: [
+    {
+      title: "What is alcohol?",
+      content:
+        "Ethyl alcohol, or ethanol, is an intoxicating ingredient found in beer, wine, and liquor. Alcohol is produced by the fermentation of yeast, sugars, and starches",
+    },
+    {
+      title: "Why do people drink alcohol?",
+      content:
+        "To enhance sociability, to increase power, to escape problems, to get drunk, for enjoyment, or for ritualistic reasons",
+    },
+    {
+      title: "What's the equivalent of one drink of alcohol?",
+      content:
+        "In the USA, one drink is equivalent to 14 grams of pure alcohol, which is generally equal to 12 oz of beer, 5 oz of wine, and 1.5 fl oz of a spirit. It may vary depending on the alcohol content of the beverage.!",
+    },
+    {
+      title: "How can I know if I may have a drinking problem?",
+      content:
+        "There are many online resources to understand what constitutes a drinking problem. You can take a test here to see if you may have a heavy drinking issue.",
+    },
+  ],
+};
+
+const HealthData = {
+  rows: [
+    {
+      title: "How long does it take for alcohol to be metabolized in the body?",
+      content:
+        "It takes more than one hour to metabolize one serving of alcohol.",
+    },
+    {
+      title: "How long will it take to feel the effects of alcohol?",
+      content:
+        "It may take an hour to metabolize a drink, but it takes approximately thirty minutes before you feel alcohol’s effects.",
+    },
+    {
+      title: "How can alcohol make me feel?",
+      content:
+        "Even though alcohol may make you feel good, it is still a depressant. It will dehydrate you.",
+    },
+    {
+      title: "What are quick ways for me to get sober while drinking?",
+      content: "You can't. The only thing that will sober you up is time.",
+    },
+    {
+      title: "Can I sleep the alcohol off if I start to feel sick?",
+      content:
+        "The worst thing a severely intoxicated person can do is lie down. If you vomit, you could actually choke and die. If you are with someone who is intoxicated and must lie down, make sure the person is lying on his/her side and his/her head is turned to the side then watch the person closely.",
+    },
+  ],
+};
+
+const DrinkData = {
+  rows: [
+    {
+      title:
+        "What does it mean to order a drink “neat”, “up” or “on the rocks?",
+      content:
+        "When you order a spirit “neat,” it means you just want the booze poured straight into the glass with no ice or added flavorings. Ordering a drink “on the rocks” will get you a straight spirit or cocktail poured over ice in a lowball glass. Ordering a drink “up” means you want it chilled and strained without ice into a stemmed cocktail glass.",
+    },
+    {
+      title: "What’s a 'twist'?",
+      content:
+        "A twist refers to a swath of citrus peel—usually lemon, orange or grapefruit with no white pith—that’s twisted over a drink.",
+    },
+    {
+      title: "What’s in simple syrup?",
+      content:
+        "Simple syrup is used to sweeten drinks, and it is easily customizable with other flavors.",
+    },
+    {
+      title: "What’s a “chaser” or “back?",
+      content:
+        "'Chaser' and 'back' are synonymous, both referring to something taken after a shot of liquor, to dull the burning sensation and cleanse the palate.",
+    },
+    {
+      title:
+        "What does it mean to order a Martini or Manhattan dry, wet, perfect, etc.?",
+      content:
+        "This refers to the amount of vermouth you want in a Martini or Manhattan. Dry doesn’t mean using dry vermouth—it means using less vermouth. Wet conversely means adding in more vermouth.",
+    },
+  ],
+};
+
+module.exports = { GeneralData, DrinkData, HealthData };
