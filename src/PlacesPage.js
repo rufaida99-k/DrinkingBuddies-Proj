439bb328c4af5756a5d00d2cd57732ae59376f67
tangym27 <mt4119@nyu.edu>
2022-11-29 13:09:30 -0500

    update style

--- src/PlacesPage.js
+++ src/PlacesPage.js
@@ -1,12 +1,10 @@
-
 // import React, { Component } from 'react';
 
 import { useState } from "react";
-import * as React from 'react';
+import * as React from "react";
 import axios from "axios";
 import Carousel from "react-material-ui-carousel";
-import Button from '@mui/material/Button';
-
+import Button from "@mui/material/Button";
 
 const divStyle = {
   margin: "15px",
@@ -16,13 +14,12 @@ const divStyle = {
   borderColor: "#d2b7e5",
   fontFamily: "Garamond",
   fontSize: "20px",
-  backgroundColor: "#dec9e9",  
-  
+  backgroundColor: "#dec9e9",
 };
 const pStyle = {
   margin: "5px",
   textAlign: "center",
-  backgroundColor: "#dec9e9",  
+  backgroundColor: "#dec9e9",
 };
 
 const pageStyle = {
@@ -37,23 +34,20 @@ const carouselStyle = {
   borderRadius: "10px",
   height: "45vh",
   margin: "10px",
-  
 };
 
 const h1Style = {
-    fontWeight: "bolder",
-}
+  fontWeight: "bolder",
+};
 
 const imgStyle = {
   width: "120px",
   height: "120px",
 };
 
-
 function App() {
   const [zipcode, setZipcode] = useState("");
-  const [places, setPlaces] = useState(['']);
-
+  const [places, setPlaces] = useState([""]);
 
   function Example(content) {
     return (
@@ -70,43 +64,47 @@ function App() {
         PrevIcon="prev" // Change the "inside of the prev button to "prev"
       >
         {content.map((item) => (
-        <Item place={item} />
-      ))}
+          <Item place={item} />
+        ))}
       </Carousel>
     );
   }
-  
+
   function Item(props) {
     return (
       <div style={pStyle}>
         <h2>{props["place"].name}</h2>
-        <p>
-          {" "}
-          Address: {props["place"].address} {" "}
-        </p>
-        <p>{props["place"].rating}</p>
+        <p> Address: {props["place"].address} </p>
+        <p> Rating: {props["place"].rating}</p>
 
         <br />
       </div>
     );
   }
 
-
   let handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      let res = await axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars%20in%20${zipcode.trim()}&key=AIzaSyDJyQraEDSqatjKqzQlMApq0DbcR73D6WA`, {
-        method: "GET"})
+      let res = await axios.get(
+        `https://maps.googleapis.com/maps/api/place/textsearch/json?query=bars%20in%20${zipcode.trim()}&key=AIzaSyDJyQraEDSqatjKqzQlMApq0DbcR73D6WA`,
+        {
+          method: "GET",
+        }
+      );
       let result = await res.data;
-      let results = result['results'].splice(0, 5)
-      let placesArray = []
-      console.log(placesArray)
+      let results = result["results"].splice(0, 5);
+      let placesArray = [];
       if (res.status === 200) {
         for (var i = 0; i < results.length; i++) {
-          placesArray.push({'name': results[i]['name'], 'address': results[i]['formatted_address'], 'rating': results[i]['rating']})
+          console.log(results[i]);
+          placesArray.push({
+            name: results[i]["name"],
+            address: results[i]["formatted_address"],
+            rating: results[i]["rating"],
+          });
         }
-        setPlaces(placesArray)
-        console.log(placesArray)
+        setPlaces(placesArray);
+        console.log(placesArray);
       } else {
         console.log("Some error occured");
       }
@@ -116,38 +114,35 @@ function App() {
   };
 
   return (
-    <><div style={pageStyle} className="App">
+    <>
+      <div style={pageStyle} className="App">
         <form onSubmit={handleSubmit}>
           <h4>Enter Your Zipcode Below To Find Bars Near You!</h4>
+          <br />
           <input
             type="text"
             value={zipcode}
             placeholder="Zipcode"
-            onChange={(e) => setZipcode(e.target.value)} />
-
-          <button type="submit">Search</button>
-
-
-          <div style={divStyle}>
-        <br />
-          <h1 style={h1Style}> Place </h1>
-
-          <div style={carouselStyle}>{Example(places ?? [])}</div>
-          <br />
+            onChange={(e) => setZipcode(e.target.value)}
+          />
           <hr />
-          {/* <Button
-            as="a"
-            variant="outline-secondary"
-            size="lg"
-            href={vodka_wiki}
-          >
-            Learn More
-          </Button> */}
-        </div>
+          <Button variant="contained" size="lg" type="submit">
+            Search
+          </Button>
+
+          {places != "" && (
+            <div style={divStyle}>
+              <h1 style={h1Style}> Places Near You </h1>
+
+              <div style={carouselStyle}>{Example(places ?? [])}</div>
+              <br />
+              <hr />
+            </div>
+          )}
         </form>
-      </div></>
+      </div>
+    </>
   );
 }
-
 
 export default App;
