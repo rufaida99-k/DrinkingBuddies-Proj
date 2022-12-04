cab0b5e61c1472774bab7cd12b2910e57889bb9e
Nicole Sosa <nls406@nyu.edu>
2022-12-04 01:08:29 -0500

    Revert "updated navigation bar"
    This reverts commit bed5af1e0001c4e2f25497abc2b7cc68397113a7.

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
       <div className="App">
         <QueryClientProvider client={queryClient}>
           <NavBar> </NavBar>
-    
 
           <Routes>
             <Route path="/" element={<HomePage />} />
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,28 @@
-import "./HomePage.css";
+// import React, { Component } from 'react';
+
+// const divStyle = {
+//   margin: "40px",
+//   height: "70vh",
+//   border: "15px solid #fda0d8",
+//   backgroundColor: "pink",
+//   display: "flex",
+//   flexWrap: "wrap",
+//   alignItems: "center",
+//   justifyContent: "center",
+// };
+// const pStyle = {
+//   fontSize: "100px",
+//   textAlign: "center",
+//   fontFamily: "Chalkduster",
+// };
+
 const HomePage = () => (
-
-    <div className = "header">
-      
-
-    </div>
+  // <div style={divStyle}>
+    // 
+    <div>
+      <p>
+    Welcome to Drinking Buddies!</p>
+  </div>
 );
 
 export default HomePage;
