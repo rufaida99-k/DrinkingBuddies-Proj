fcf0d5f149932016f0dd24390ac0cb6e23ac0a4c
Rufaida Khan <rufaida.99khan@gmail.com>
2022-12-04 01:27:50 -0500

    Quiz Related Revamp

--- src/QuizPage.js
+++ src/QuizPage.js
@@ -4,27 +4,18 @@ import "./quiz.css";
 import DrinkData from "./QuizData.js";
 import { Link } from "react-router-dom";
 
-const divStyle = {
-  flexWrap: "wrap",
-  alignItems: "center",
-  justifyContent: "center",
-  backgroundColor: "#22277A",
-  height: "90vh",
-};
-
-// const manhattanDrink = require("./img/ManhattanDrink.png");
 const drinkStyle = {
   width: "128px",
   height: "128px",
 };
 
 const drinks = {
-  manhattan: 0,
-  cosmopolitan: 0,
-  daiquiri: 0,
-  old_fashioned: 0,
-  margarita: 0,
-  gin_and_tonic: 0,
+  Manhattan: 0,
+  Cosmopolitan: 0,
+  Daiquiri: 0,
+  Old_Fashioned: 0,
+  Margarita: 0,
+  Gin_And_Tonic: 0,
 };
 
 let drinkingResultDrink = "";
@@ -50,32 +41,60 @@ export default function HomePage() {
   const [currQuestion, setCurrQuestion] = useState(0);
 
   const questions = [
+    /*
+
+    Money Question?!
+
+    */
+
     {
       text: "How strong do you want your drink?",
       options: [
-        { id: 0, text: "Strong", drinks: ["manhattan", "cosmopolitan"] },
+        { id: 0, text: "Strong", drinks: ["Manhattan", "Cosmopolitan"] },
         {
           id: 1,
           text: "In Between",
-          drinks: ["old_fashioned", "gin_and_tonic", "margarita"],
+          drinks: ["Old_Fashioned", "Gin_And_Tonic", "Margarita"],
         },
-        { id: 2, text: "Light", drinks: ["daiquiri"] },
+        { id: 2, text: "Light", drinks: ["Daiquiri"] },
       ],
     },
+
+    {
+      text: "Is This A Party Of One Or Many?",
+      options: [
+        { id: 0, text: "It's Me, Myself & I", drinks: ["Manhattan", "Old_Fashioned"] },
+        { id: 1, text: "We Are Going All Out Tonight!", drinks: ["Cosmopolitan", "Daiquiri", "Gin_And_Tonic", "Margarita"] },
+      ],
+    },
+
+    {
+      text: "Do We Like Slushies?",
+      options: [
+        { id: 0, text: "Yes, Absolutely!", drinks: ["Daiquiri", "Margarita"] },
+        {
+          id: 1,
+          text: "Sometimes When It's Hot Out",
+          drinks: ["Old_Fashioned", "Manhattan","Cosmopolitan"],
+        },
+        { id: 2, text: "No, What Is Wrong With You?", drinks: ["Gin_And_Tonic"] },
+      ],
+    },
+
     {
       text: "How do you take your coffee?",
       options: [
         {
           id: 0,
           text: "No milk and no sugar!",
-          drinks: ["manhattan", "gin_and_tonic"],
+          drinks: ["Manhattan", "Gin_And_Tonic"],
         },
         {
           id: 1,
           text: "Some milk and sugar",
-          drinks: ["old_fashioned", "daiquiri", "margarita"],
+          drinks: ["Old_Fashioned", "Daiquiri", "Margarita"],
         },
-        { id: 2, text: "Lots of milk and sugar", drinks: ["cosmopolitan"] },
+        { id: 2, text: "Lots of milk and sugar", drinks: ["Cosmopolitan"] },
       ],
     },
 
@@ -85,13 +104,13 @@ export default function HomePage() {
         {
           id: 0,
           text: "I'd Never Pass On A Tart Cherry",
-          drinks: ["daiquiri", "cosmopolitan", "margarita"],
+          drinks: ["Daiquiri", "Cosmopolitan", "Margarita"],
         },
-        { id: 1, text: "No Thank You Berry Much ", drinks: ["manhattan"] },
+        { id: 1, text: "No Thank You Berry Much ", drinks: ["Manhattan"] },
         {
           id: 2,
           text: "I Find Them Ap-peeling Sometimes",
-          drinks: ["old_fashioned", "gin_and_tonic"],
+          drinks: ["Old_Fashioned", "Gin_And_Tonic"],
         },
       ],
     },
@@ -101,10 +120,10 @@ export default function HomePage() {
         {
           id: 0,
           text: "Fruity (Lime, Cherry, etc.)",
-          drinks: ["manhattan", "cosmopolitan", "old_fashioned", "daiquiri"],
+          drinks: ["Manhattan", "Cosmopolitan", "Old_Fashioned", "Daiquiri"],
         },
-        { id: 1, text: "Leaves", drinks: ["manhattan", "gin_and_tonic"] },
-        { id: 2, text: "Salty Rim", drinks: ["margarita"] },
+        { id: 1, text: "Leaves", drinks: ["Manhattan", "Gin_And_Tonic"] },
+        { id: 2, text: "Salty Rim", drinks: ["Margarita"] },
       ],
     },
     {
@@ -113,18 +132,18 @@ export default function HomePage() {
         {
           id: 0,
           text: "Yes, New York Is Always On My Mind",
-          drinks: ["manhattan"],
+          drinks: ["Manhattan"],
         },
 
         {
           id: 1,
           text: "No, Anywhere But Here",
           drinks: [
-            "cosmopolitan",
-            "old_fashioned",
-            "daiquiri",
-            "gin_and_tonic",
-            "margarita",
+            "Cosmopolitan",
+            "Old_Fashioned",
+            "Daiquiri",
+            "Gin_And_Tonic",
+            "Margarita",
           ],
         },
       ],
@@ -147,43 +166,47 @@ export default function HomePage() {
   };
 
   return (
-    <div style={divStyle}>
-      <h1 id="Heading1"> Drinking Buddy Quiz </h1>
+    <div className="main-Quiz">
+      <br></br><h1 class="quizHeading1"> Drinking Buddy Quiz </h1><br></br>
+      <div className="blurQuiz" align="center">
+      
       {displayResults ? (
-        <div className="results">
-          <h2 id="Heading2"> {drinkingResultDrink}</h2>
-          <h3 id="Heading3"> {drinkingResultDescription}</h3>
-          <img style={drinkStyle} ALT="Drink" src={drinkingResultImg} />
+        <div className="quizResults" align="center">
+          <h3 className="quizHeading3" align="center"> {drinkingResultDrink}</h3>
+          <br></br><h4 className="quizHeading4" align="center"> {drinkingResultDescription} </h4>
+          <img style={drinkStyle} align="center" alt="Drink Result" src={drinkingResultImg} />
           <hr />
           <Button as="a" variant="outline-secondary" size="lg">
-            <Link className="text-link" to="/list">
+            <Link className="quiz-Links" to="/list">
               Learn More
             </Link>
           </Button>
           {"  "}
           <Button as="a" variant="outline-secondary" size="lg">
-            <Link className="text-link" to="/list">
+            <Link className="quiz-Links" to="/quiz"> 
               Try Again
             </Link>
           </Button>
         </div>
       ) : (
-        <div className="question-container">
-          <h2 className="question-number"> Question {currQuestion + 1} </h2>
-          <h3 className="question-quest"> {questions[currQuestion].text}</h3>
-          <ul id="horizontal">
+        <div className="quizQuestion" align="center">
+          <br></br><h2 className="questionNumber" align="center"> Question {currQuestion + 1} </h2>
+          <br></br><h3 className="questionContents" align="center"> {questions[currQuestion].text}</h3>
+          
             {questions[currQuestion].options.map((option) => {
               return (
-                <li onClick={() => optionClick(option)} key={option.id}>
-                  {option.text}
-                </li>
+                <p class="choices" onClick={() => optionClick(option)} key={option.id}>
+                  {option.text} 
+                </p>
               );
             })}
-          </ul>
+          
         </div>
       )}
 
       <br />
+     
+      </div>
     </div>
   );
 }
