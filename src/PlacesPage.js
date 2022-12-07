142d18853e5aa508bb45c60bbcb0ba6e0ca98a34
Rufaida Khan <rufaida.99khan@gmail.com>
2022-12-07 17:40:26 -0500

    places near me revamp

--- src/PlacesPage.js
+++ src/PlacesPage.js
@@ -5,67 +5,38 @@ import * as React from "react";
 import axios from "axios";
 import Carousel from "react-material-ui-carousel";
 import Button from "@mui/material/Button";
+import "./placesNearMe.css";
 
-const divStyle = {
-  margin: "15px",
-  width: "70vh",
-  height: "55vh",
-  borderRadius: "20px",
-  borderColor: "#d2b7e5",
-  fontFamily: "Garamond",
-  fontSize: "20px",
-  backgroundColor: "#dec9e9",
-};
-const pStyle = {
-  margin: "5px",
-  textAlign: "center",
-  backgroundColor: "#dec9e9",
-};
-
-const pageStyle = {
-  height: "90vh",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-};
-
-const carouselStyle = {
-  border: "10px solid #e5d9f2",
-  padding: "10px",
-  borderRadius: "10px",
-  height: "40vh",
-  margin: "10px",
-};
-
-const h1Style = {
-  fontWeight: "bolder",
-};
-
-const imgStyle = {
-  width: "100px",
-  height: "100px",
-  border: "5px solid black",
-  borderRadius: "10px",
-  padding: "10px",
-};
 
 function App() {
   const [zipcode, setZipcode] = useState("");
   const [places, setPlaces] = useState([""]);
+
+  const cabbyAnimation = require("./placesNearMe-Images/taxi-animation.gif");  
+  const barIcon = require("./placesNearMe-Images/bar.png");
 
   function Example(content) {
     return (
       <Carousel
         NavButton={({ onClick, className, style, next, prev }) => {
           return (
-            <Button onClick={onClick} id="buttonStyle">
+            <Button onClick={onClick}  className="next-prev-button" style={{
+              marginTop: "250%",
+              color: "white",
+              fontFamily: "Eczar-Medium",
+              fontWeight: "200",
+              backgroundColor: "#66222e",
+              borderRadius: "20px",
+              opacity: "1",
+            }}>
               {next && "Next"}
               {prev && "Prev"}
+              {/* style={{backgroundColor: "rgb(255,0,255)"}} */}
             </Button>
           );
         }}
-        NextIcon="next" // Change the "inside" of the next button to "next"
-        PrevIcon="prev" // Change the "inside of the prev button to "prev"
+        NextIcon="Next" // Change the "inside" of the next button to "next"
+        PrevIcon="Prev" // Change the "inside of the prev button to "prev"
       >
         {content.map((item) => (
           <Item place={item} />
@@ -76,11 +47,14 @@ function App() {
 
   function Item(props) {
     return (
-      <div style={pStyle}>
+      <div className="PNM-Carousel" align="center">
+        <img className="imageStyle-PNM" alt="Icon" src={barIcon} />
+        <br></br>
+        <br></br>
         <h2>{props["place"].name}</h2>
         <p> Address: {props["place"].address} </p>
         <p> Rating: {props["place"].rating}</p>
-        <img style={imgStyle} alt="Icon" src={"https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/bar-71.png"} />
+        
 
         <br />
       </div>
@@ -121,30 +95,33 @@ function App() {
 
   return (
     <>
-      <div style={pageStyle} className="App">
+      <div className="placesNearMeStyle" align="center">
+        <br></br>
+        <div className="PNM-HEADING-TITLE">
+          <h1 className="Places-Near-Me-Heading-Title"> Places Near Me </h1>
+        </div>
+        <div className="cabbyAnimation-Div" align="center">
+          <img src={cabbyAnimation} style={{borderRadius: "20px"}}  alt="Taxi Animation"></img>
+        </div>
         <br />
-        <form onSubmit={handleSubmit}>
-          <h4>Enter Your Zipcode Below To Find Bars Near You!</h4>
+        <h4 className="PNM-Zipcode">Enter Your Zipcode Below To Find Bars Near You!</h4>
           <br />
-          <input
-            type="text"
-            value={zipcode}
-            placeholder="Zipcode"
-            onChange={(e) => setZipcode(e.target.value)}
-          />
+        <form style={{width: "40%" }} onSubmit={handleSubmit}>
+          <input type="text" className="zipcode-NPM" value={zipcode} placeholder="Zipcode" onChange={(e) => setZipcode(e.target.value)}/>
           <hr />
-          <div class="vertical-center">
-          <Button variant="contained" size="lg" type="submit">
+          <div class="Explore-PNM" align="center">
+          {/* <Button className="button-style-NPM" variant="contained" size="lg" type="submit">
             Search
-          </Button>
+          </Button> */}
+
+          <button className="NPM-ZipCode-Button-Style" type="submit">Search</button>
           </div>
-
+          <br></br>
           {places.length > 1 && (
-            <div style={divStyle}>
-              <br />
-              <h1 style={h1Style}> Places Near You </h1>
-
-              <div style={carouselStyle}>{Example(places ?? [])}</div>
+            
+            <div className="PNM-Div" align="center">
+              <br></br>
+              <div className="PNM-Carousel" align="center">{Example(places ?? [])}</div>
             </div>
           )}
         </form>
